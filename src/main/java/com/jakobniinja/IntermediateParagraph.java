21a4418ece135130b82ae66654bab7aaaacb2fde
jakobniinja <dansaren124@outlook.com>
2024-04-27 07:41:24 +0200

    Refactor test and Impl add previous animals

--- src/main/java/com/jakobniinja/IntermediateParagraph.java
+++ src/main/java/com/jakobniinja/IntermediateParagraph.java
@@ -9,13 +9,18 @@ public class IntermediateParagraph implements Paragraph {
 
   final String introVerse = "The green frog jumped over the ...";
 
-  final String previousAnimal = "Tiger";
+  final String[] previousAnimal = new String[]{"Tiger", "Dragon"};
 
   @Override
   public List<String> generateContent() {
-    return new ArrayList() {{
-      add(introVerse);
-      add("There was an old lady who swallowed a " + animal);
-    }};
+    List<String> result = new ArrayList<>();
+
+    result.add(introVerse);
+    result.add("There was an old lady who swallowed a " + animal);
+
+    for (int i = previousAnimal.length - 1; i >= 1; i--) {
+      result.add("I don't know why she swallowed a " + previousAnimal[i] + " - perhaps she'll die!");
+    }
+    return result;
   }
 }
--- src/test/java/com/jakobniinja/IntermediateParagraphTest.java
+++ src/test/java/com/jakobniinja/IntermediateParagraphTest.java
@@ -1,47 +1,33 @@
 package com.jakobniinja;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Collections;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IntermediateParagraphTest {
 
+  private IntermediateParagraph ip;
+
+  @BeforeEach
+  void setUp() {
+    ip = new IntermediateParagraph();
+  }
+
   @Test
   void testInit() {
-    assertNotNull(new IntermediateParagraph());
+    assertNotNull(ip);
   }
 
   @Test
-  void testFrog() {
-    IntermediateParagraph ip = new IntermediateParagraph();
-    assertEquals(ip.animal, "Frog");
-  }
-
-
-  @Test
-  void testInitVerse() {
-    IntermediateParagraph ip = new IntermediateParagraph();
-    assertEquals(ip.introVerse, "The green frog jumped over the ...");
+  void testTiger() {
+    assertTrue(Arrays.asList(ip.previousAnimal).contains("Tiger"));
   }
 
   @Test
-  void testPrevious() {
-    IntermediateParagraph ip = new IntermediateParagraph();
-    assertEquals(ip.previousAnimal, "Tiger");
-  }
-
-  @Test
-  void testContainsFrog() {
-    IntermediateParagraph ip = new IntermediateParagraph();
-    assertTrue(ip.generateContent().contains("There was an old lady who swallowed a Frog"));
-  }
-
-  @Test
-  void testContainsVerse() {
-    IntermediateParagraph ip = new IntermediateParagraph();
-    assertTrue(ip.generateContent().contains("The green frog jumped over the ..."));
+  void testDragon() {
+    assertTrue(ip.generateContent().toString().contains("swallowed a Dragon"));
   }
 }
