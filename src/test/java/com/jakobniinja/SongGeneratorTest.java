5ca8c2975c637fb93475a05c1c92d1347d3f674f
jakobniinja <dansaren124@outlook.com>
2024-05-02 06:55:21 +0200

    Add build with selection test

--- src/test/java/com/jakobniinja/SongGeneratorTest.java
+++ src/test/java/com/jakobniinja/SongGeneratorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,47 @@ class SongGeneratorTest {
   @Test
   void testBuild() {
     SongGenerator generator = new SongGenerator(new ArrayList<>());
-    List<Paragraph> build = generator.build("Snake", "Jaguar ", "Wolf", "Spider", "Lion", "Monkey", "Turtle");
+    List<Paragraph> build = generator.build("Snake", "Jaguar", "Wolf", "Spider", "Lion", "Monkey", "Turtle");
     assertEquals(7, build.size());
+  }
+
+  @Test
+  void testBuildWithSelect() {
+    SongGenerator generator = new SongGenerator(new ArrayList<>());
+    String[] animals = {"Snake", "Jaguar", "Wolf", "Spider", "Lion", "Monkey", "Turtle"};
+
+    List<Paragraph> build = generator.build(animals);
+
+    List<String> previous = build.stream()
+        .filter(IntermediateParagraph.class::isInstance)
+        .map(IntermediateParagraph.class::cast)
+        .map(p -> Arrays.toString(p.previousAnimal))
+        .toList();
+
+    String[] expectedIntermediate = {"Snake", "Jaguar"};
+    assertEquals(Arrays.toString(expectedIntermediate), previous.get(0));
+  }
+
+  @Test
+  void testBuildWithSelect2() {
+    SongGenerator generator = new SongGenerator(new ArrayList<>());
+    String[] animals = {"Snake", "Jaguar", "Wolf", "Spider", "Lion", "Monkey", "Turtle"};
+
+    List<Paragraph> build = generator.build(animals);
+
+    List<String> previous = build.stream()
+        .filter(IntermediateParagraph.class::isInstance)
+        .map(IntermediateParagraph.class::cast)
+        .map(p -> Arrays.toString(p.previousAnimal))
+        .toList();
+
+    String[] expectedIntermediate = {"Snake", "Jaguar", "Wolf"};
+    assertEquals(Arrays.toString(expectedIntermediate), previous.get(1));
+  }
+
+  @Test
+  void testSelection() {
+    String[] selected = SongGenerator.select(new String[]{"Snake", "Jaguar", "Spider"}, 3);
+    assertEquals(4, selected.length);
   }
 }
